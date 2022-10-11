052e2f1424171e545dfde40c9ab8b37ebddd3c70
DDomix <dominik.papp1010@gmail.com>
2022-10-11 13:05:31 +0200

    heloka

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+class Szazlabu{
+    #labakszama
+    constructor(labakszama){
+        this.#labakszama=labakszama;
+    }
+    toString(){
+        return this.#labakszama+ " lábú százlábú";
+    }
+}
 document.addEventListener("DOMContentLoaded",()=>{
-    console.log("loaded");
+    let szamok=[];
+    for (let i = 0; i < 20; i++) {
+        let veletlen=Math.floor(Math.random()*19)+1;
+        szamok.push(veletlen);
+    }
+    console.log(szamok);
+    
+    document.getElementById("valogat").addEventListener("click",()=>{
+        let otteloszthato=szamok.filter(e=>e%5===0);
+        console.log(otteloszthato);
+    })
+    document.getElementById("hozzaad").addEventListener("click",()=>{
+        let szam=ParseInt(document.getElementById("szam").value);
+        szamok.push(szam);
+    })
 })
